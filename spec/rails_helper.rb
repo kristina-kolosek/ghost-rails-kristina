4d0b5e651faf49a2ff73d275be483f4cff00519e
Kristina G <kristina@kolosek.com>
2018-01-11 11:40:24 +0100

    [#GB1] Initialized rails app, added rspec, rubocop and railsconfig

---
+++ spec/rails_helper.rb
@@ -0,0 +1,46 @@
+require "spec_helper"
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../../config/environment", __FILE__)
+# Prevent database truncation if the environment is production
+abort("Environment is running in production mode!") if Rails.env.production?
+require "rspec/rails"
+# Add additional requires below this line. Rails is not loaded until this point!
+require "capybara/rspec"
+# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+# Checks for pending migrations and applies them before tests are run.
+ActiveRecord::Migration.maintain_test_schema!
+
+Capybara.register_driver :selenium_chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.javascript_driver = :selenium_chrome
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  # Filter lines from Rails gems in backtraces.
+  config.filter_rails_from_backtrace!
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,49 @@
+# The generated `.rspec` file contains `--require spec_helper` which will cause
+# this file to always be loaded, without a need to explicitly require it in any
+# files.
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    # This option makes the `description` and `failure_message` of custom
+    # matchers include text for helper methods defined using `chain`
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  # rspec-mocks config goes here.
+  config.mock_with :rspec do |mocks|
+    # Prevents you from mocking or stubbing a method that does not exist on
+    # a real object.
+    mocks.verify_partial_doubles = true
+  end
+
+  # This option causes shared context metadata to be inherited by the
+  # metadata hash of host groups and examples, rather than
+  # triggering implicit auto-inclusion in groups with matching metadata.
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  # This allows you to limit a spec run to individual examples or groups
+  # you care about by tagging them with `:focus` metadata. When nothing
+  # is tagged with `:focus`, all examples get run.
+  config.filter_run_when_matching :focus
+
+  # Limits the available syntax to the non-monkey patched syntax that is
+  # recommended.
+  config.disable_monkey_patching!
+
+  if config.files_to_run.one?
+    # Use the documentation formatter for detailed output,
+    # unless a formatter has already been configured
+    config.default_formatter = "doc"
+  end
+
+  # Print the 10 slowest examples and example groups at the
+  # end of the spec run, to help surface which specs are running
+  # particularly slow.
+  config.profile_examples = 10
+
+  # Run specs in random order to surface order dependencies.
+  config.order = :random
+
+  # Seed global randomization in this process using the `--seed` CLI option.
+  Kernel.srand config.seed
+end
